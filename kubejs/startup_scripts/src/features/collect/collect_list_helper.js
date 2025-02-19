25f21819b6abdcb43c962a0b8f6a074acdc496e3
khazenor <80185732+khazenor@users.noreply.github.com>
2025-02-18 21:33:48 -0600

    add byCollection

--- kubejs/startup_scripts/src/features/collect/collect_list_helper.js
+++ kubejs/startup_scripts/src/features/collect/collect_list_helper.js
@@ -1,14 +1,26 @@
 const CollectListHelper = {
   get allCollectibleIds() {
+    let byCollection = this.byCollection
     let collectibleIds = []
+    for (let collectionId in byCollection) {
+      collectibleIds = collectibleIds.concat(byCollection[collectionId])
+    }
+    return collectibleIds
+  },
+  get byCollection () {
+    let byCollection = {}
     for (let collectionId in CollectLists) {
+      let collectibleIds = []
       let collection = CollectLists[collectionId]
       let subCollections = collection.subCollections
+      
       for (let subCollectionId in subCollections) {
         let subCollection = subCollections[subCollectionId]
         collectibleIds = collectibleIds.concat(subCollection.list)
       }
+
+      byCollection[collectionId] = collectibleIds
     }
-    return collectibleIds
+    return byCollection
   }
 }
