ff06b073224d00d80df1d48da1f0ad8add60bba1
khazenor <80185732+khazenor@users.noreply.github.com>
2025-02-25 17:23:07 -0600

    cached functions for milestones

--- kubejs/startup_scripts/src/features/collect/collect_caches.js
+++ kubejs/startup_scripts/src/features/collect/collect_caches.js
@@ -11,6 +11,20 @@ const CollectCaches = {
       () => CollectListHelper.categoryNames
     )
   },
+  isNumberAMilestoneForCategory(number, categoryId) {
+    return Object.keys(
+      this._rewardsByMilestonesForCategory(categoryId)
+    ).includes(number)
+  },
+  rewardForCategoryMilestone (milestoneNumber, categoryId) {
+    return this._rewardsByMilestonesForCategory(categoryId)[milestoneNumber]
+  },
+  _rewardsByMilestonesForCategory(categoryId) {
+    return this._getAndReturnCache(
+      `_rewardsByMilestonesForCategory_${categoryId}`,
+      () => CollectMilestones._rewardsByMilestonesForCategory(categoryId)
+    )
+  },
   _getAndReturnCache(key, callBackFunc) {
     if (!this.caches[key]) {
       this.caches[key] = callBackFunc()
