3f4cb3a7018ac01d728adf7f37dbc0bd683cee38
khazenor <80185732+khazenor@users.noreply.github.com>
2025-02-24 22:24:08 -0600

    modify cached value to be caching on demand

--- kubejs/startup_scripts/src/features/collect/collect_caches.js
+++ kubejs/startup_scripts/src/features/collect/collect_caches.js
@@ -3,6 +3,24 @@ const CollectCaches = {
     this.categoryLists = CollectListHelper.byCategory
     this.categoryNames = CollectListHelper.categoryNames
   },
-  categoryLists: null,
-  categoryNames: null
+  get categoryLists () {
+    return this.getAndReturnCache(
+      'categoryLists',
+      () => CollectListHelper.byCategory
+    )
+  },
+  get categoryNames () {
+    return this.getAndReturnCache(
+      'categoryNames',
+      () => CollectListHelper.categoryNames
+    )
+  },
+  getAndReturnCache(key, callBackFunc) {
+    if (!this.caches[key]) {
+      this.caches[key] = callBackFunc()
+      
+    }
+    return this.caches[key]
+  },
+  caches: {}
 }
