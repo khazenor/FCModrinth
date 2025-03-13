cc92075a7e2091b36d61045c7f2d387be5916b72
khazenor <80185732+khazenor@users.noreply.github.com>
2025-03-12 23:00:50 -0500

    flora collection

--- kubejs/startup_scripts/src/features/collect/gui/collect_gui_main_menu.js
+++ kubejs/startup_scripts/src/features/collect/gui/collect_gui_main_menu.js
@@ -2,14 +2,21 @@ const CollectGuiMainMenu = {
   openMainMenu (event) {
     let mainMenu = new MenuWrapper(Text.translate('collect.gui.name.mainMenu'))
 
-    mainMenu.addSlot({
-      label:CollectTransHelper.categoryName('entities'),
-      item: CollectLists['entities'].icon,
-      onLeftClicked: (_clickedEvent) => {
-        CollectGuiSubMenus.openSubMenuForCategory(event, 'entities')
-      }
-    })
+    for (let collectionId in CollectLists) {
+      CollectGuiMainMenu_AddMenu(event, mainMenu, collectionId)
+    }
 
     mainMenu.menu.show(event.player)
   }
+}
+
+const CollectGuiMainMenu_AddMenu = (event, menu, collectionId) => {
+  menu.addSlot({
+    label:CollectTransHelper.categoryName(collectionId),
+    item: CollectLists[collectionId].icon,
+    onLeftClicked: (_clickedEvent) => {
+      CollectGuiSubMenus.openSubMenuForCategory(event, collectionId)
+    }
+  })
+
 }
