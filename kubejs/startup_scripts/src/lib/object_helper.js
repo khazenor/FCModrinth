6871bd7b3453b13b4e247e11015bd6b584f48446
khazenor <80185732+khazenor@users.noreply.github.com>
2025-03-17 12:19:51 -0500

    object helper func

--- kubejs/startup_scripts/src/lib/object_helper.js
+++ kubejs/startup_scripts/src/lib/object_helper.js
@@ -5,5 +5,19 @@ const ObjectHelper = {
       newObj[`${key}`] = obj[key]
     }
     return newObj
+  },
+  getNestedValue (object, keysString) {
+    let keys = keysString.split('/')
+    let curObj = object
+    for (let key of keys) {
+      if (typeof curObj !== 'object') {
+        return null
+      } else if (key in curObj) {
+        curObj = curObj[key]
+      } else {
+        return null
+      }
+    }
+    return curObj
   }
 }
