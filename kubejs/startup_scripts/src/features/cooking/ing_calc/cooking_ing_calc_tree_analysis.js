aee52b9f80ebc388d4e4e101d3f6dc7a5b28a5f6
khazenor <80185732+khazenor@users.noreply.github.com>
2025-03-19 21:12:30 -0500

    another failing impl

--- kubejs/startup_scripts/src/features/cooking/ing_calc/cooking_ing_calc_tree_analysis.js
+++ kubejs/startup_scripts/src/features/cooking/ing_calc/cooking_ing_calc_tree_analysis.js
@@ -1,12 +1,13 @@
 const CookingIngCalcTreeAnalysis = {
+  baseIngTree: {},
   analyzeTree (recipeTree, targetOutputs, baseIngs) {
-    let baseIngTree = this._firstPassForTargetOutputs(recipeTree, targetOutputs)
+    this.baseIngTree = this._firstPassForTargetOutputs(recipeTree, targetOutputs)
 
-    for (let output in baseIngTree) {
-      this._toBaseIngListRecur(baseIngTree[output], baseIngs)
+    for (let output in this.baseIngTree) {
+      this._toBaseIngListRecur(this.baseIngTree[output], baseIngs, recipeTree)
     }
 
-    return baseIngTree
+    return this.baseIngTree
   },
   _firstPassForTargetOutputs (recipeTree, targetOutputs) {
     let baseIngTree = {}
@@ -21,27 +22,33 @@ const CookingIngCalcTreeAnalysis = {
     }
     return baseIngTree
   },
-  _toBaseIngListRecur (ingObj, baseIngs) {
-    if (ArrayHelper.isArray(ingObj)) {
+  _toBaseIngListRecur (ingObj, baseIngs, recipeTree) {
+    if (Array.isArray(ingObj)) {
       for (let i = 0; i < ingObj.length; i++) {
         let ingVal = ingObj[i]
-        if (!baseIngs.includes(ingVal)) {
-          let newIngs = this._lookUpHighestLevelIngForOutput(ingVal)
-          if (newIngs) {
-            ingObj[i] = newIngs
-          }
-        } else if (ArrayHelper.isArray(ingVal)) {
-          this._toBaseIngListRecur(ingVal)
-        }
+        this._toBaseIngListRecur(ingVal)
+        
+      }
+    } else if (!baseIngs.includes(ingObj)) {
+      let newIngs = this._lookUpHighestLevelIngForOutput(ingObj, recipeTree)
+      if (newIngs) {
+        ArrayHelper.addToObjectArray(this.baseIngTree, ingVal, newIngs)
       }
     }
   },
-  _lookUpHighestLevelIngForOutput(output) {
+  _lookUpHighestLevelIngForOutput(output, recipeTree) {
     for (let i = recipeTree.length - 1; i >= 0; i --) {
       let curRecipeTreeLayer = recipeTree[i]
       for (let recipeOutput in curRecipeTreeLayer) {
+        console.log('output')
+        console.log(output)
+        console.log('recipeOutput')
+        console.log(recipeOutput)
         let ings = curRecipeTreeLayer[recipeOutput]
         if (recipeOutput === output) {
+          console.log('recipeOutput === output')
+          console.log(output)
+          console.log(ings)
           return ings
         }
       }
