cd4d4ec19f599a51f032f4117728316687cba22c
khazenor <80185732+khazenor@users.noreply.github.com>
2025-03-21 21:38:46 -0500

    remove logging and update is str

--- kubejs/startup_scripts/src/features/cooking/ing_calc/cooking_ing_calc_tree_analysis.js
+++ kubejs/startup_scripts/src/features/cooking/ing_calc/cooking_ing_calc_tree_analysis.js
@@ -37,40 +37,27 @@ const CookingIngCalcTreeAnalysis = {
     let nextPassTree = {}
     for (let output in analyzeTree) {
       let ings = analyzeTree[output]
-      nextPassTree[output] = this.nextPassIngsRecur(ings, baseIngs, recipeTree, 
-        false
-      )
+      nextPassTree[output] = this._nextPassIngsRecur(ings, baseIngs, recipeTree)
     }
     if (toCache) {
       CacheHelperConst.cacheObject(this.cacheFileName(level), nextPassTree)
     }
     return nextPassTree
   },
-  nextPassIngsRecur (ingObj, baseIngs, recipeTree, doLog) {
-    if (doLog) {
-      console.log('ingObj')
-      console.log(typeof ingObj)
-      console.log(ingObj)
-    }
+  _nextPassIngsRecur (ingObj, baseIngs, recipeTree) {
     if (this.isStr(ingObj)) {
-      if (doLog) {
-        console.log(`isStr:${ingObj}`)
-      }
       if (baseIngs.includes(`${ingObj}`)) {
         return `${ingObj}`
       } else {
-        let ingsOptions = this.ingsOptionsForOutput(ingObj, recipeTree, doLog)
+        let ingsOptions = this._ingsOptionsForOutput(ingObj, recipeTree)
         if (ingsOptions.length > 0) {
           return ingsOptions
         }
       }
     } else { // array
-      if (doLog) {
-        console.log(`array:${ingObj}`)
-      }
       let returnList = []
       for (let ingObjChild of ingObj) {
-        let nextVal = this.nextPassIngsRecur(ingObjChild, baseIngs, recipeTree, doLog)
+        let nextVal = this._nextPassIngsRecur(ingObjChild, baseIngs, recipeTree)
         if (nextVal) {
           returnList.push(nextVal)
         }
@@ -78,7 +65,7 @@ const CookingIngCalcTreeAnalysis = {
       return returnList
     }
   },
-  ingsOptionsForOutput(output, recipeTree, doLog) {
+  _ingsOptionsForOutput(output, recipeTree) {
     let ingsOptions = []
     for (let i = 0; i < recipeTree.length; i ++) {
       let curRecipeTreeLayer = recipeTree[i]
@@ -89,12 +76,6 @@ const CookingIngCalcTreeAnalysis = {
         }
       }
     }
-    if (doLog) {
-      console.log('ingsOptionsForOutput')
-      console.log(output)
-      console.log('ingOptions')
-      console.log(ingsOptions)
-    }
     return ingsOptions
   },
   isStr(ingObj) {
@@ -103,67 +84,9 @@ const CookingIngCalcTreeAnalysis = {
       StrHelper.isStr(ingObj) ||
       (
         typeof ingObj === 'object' &&
-        ingObj[0].length === 1
+        ingObj[0].length === 1 &&
+        typeof ingObj[0] === 'string'
       )
     )
   }
-  // analyzeTree (recipeTree, targetOutputs, baseIngs) {
-  //   this.baseIngTree = this._firstPassForTargetOutputs(recipeTree, targetOutputs)
-
-  //   for (let output in this.baseIngTree) {
-  //     this._toBaseIngListRecur(this.baseIngTree[output], baseIngs, recipeTree)
-  //   }
-
-  //   return this.baseIngTree
-  // },
-  // _firstPassForTargetOutputs (recipeTree, targetOutputs) {
-  //   let baseIngTree = {}
-  //   for (let i = recipeTree.length - 1; i >= 0; i --) {
-  //     let curRecipeTreeLayer = recipeTree[i]
-  //     for (let recipeOutput in curRecipeTreeLayer) {
-  //       let ings = curRecipeTreeLayer[recipeOutput]
-  //       if (targetOutputs.includes(recipeOutput)) {
-  //         ArrayHelper.addToObjectArray(baseIngTree, recipeOutput, ings)
-  //       }
-  //     }
-  //   }
-  //   return baseIngTree
-  // },
-  // _toBaseIngListRecur (ingObj, baseIngs, recipeTree) {
-  //   if (Array.isArray(ingObj)) {
-  //     for (let i = 0; i < ingObj.length; i++) {
-  //       let ingVal = ingObj[i]
-  //       this._toBaseIngListRecur(ingVal)
-        
-  //     }
-  //   } else if (!baseIngs.includes(ingObj)) {
-  //     let newIngs = this.ingsOptionsForOutput(ingObj, recipeTree)
-  //     if (newIngs) {
-  //       ArrayHelper.addToObjectArray(this.baseIngTree, ingVal, newIngs)
-  //     }
-  //   }
-  // },
-  // _incorporateRecipeInformation (baseIngTree, output, ings, baseIngs, recipeTree) {
-  //   for (let treeOutput in baseIngTree) {
-  //     this._replaceIngWithIngArrayRecur(
-  //       baseIngTree[treeOutput],
-  //       output,
-  //       ings,
-  //       baseIngs,
-  //       recipeTree
-  //     )
-  //   }
-  // },
-  // _replaceIngWithIngArrayRecur (ingObj, targetIng, ingArray, baseIngs, recipeTree) {
-  //   if (ArrayHelper.isArray(ingObj)) {
-  //     for (let i = 0; i < ingObj.length; i++) {
-  //       let ingVal = ingObj[i]
-  //       if (ingVal === targetIng) {
-  //         ingObj[i] = ingArray
-  //       } else if (ArrayHelper.isArray(ingVal)) {
-  //         this._replaceIngWithIngArrayRecur(ingVal)
-  //       }
-  //     }
-  //   }
-  // }
 }
