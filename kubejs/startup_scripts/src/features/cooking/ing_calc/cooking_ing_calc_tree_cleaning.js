50b73eb35cf6765fab558bd1e84739f39a801cc6
khazenor <80185732+khazenor@users.noreply.github.com>
2025-03-22 10:26:02 -0500

    clean tree logic

--- kubejs/startup_scripts/src/features/cooking/ing_calc/cooking_ing_calc_tree_cleaning.js
+++ kubejs/startup_scripts/src/features/cooking/ing_calc/cooking_ing_calc_tree_cleaning.js
@@ -16,22 +16,45 @@ const CookingIngCalcTreeCleaning = {
     return cleanedBaseIngTreeByOutputs
   },
   _cleanRawBaseIngTreeRecur (ingObj) {
-    if (
-      StrHelper.isStrRobust(ingObj) &&
-      !this._workingVisitedIds.includes(`${ingObj}`)
-    ) {
-      this._workingBaseIngTree.push(`${ingObj}`)
-      this._workingVisitedIds.push(`${ingObj}`)
+    if (StrHelper.isStrRobust(ingObj)) {
+      if (!this._workingVisitedIds.includes(`${ingObj}`)) {
+        this._workingBaseIngTree.push(`${ingObj}`)
+        this._workingVisitedIds.push(`${ingObj}`)
+      }
     } else {
-      let ingArray = ArrayHelper.toArray(ingObj)
-      if (ingArray && ingArray.length > 1 && StrHelper.isStrRobust(ingArray[0])) {
-        this._workingBaseIngTree.push(ingObj)
+      if (this._ingObjIsStrArray(ingObj) && ingObj.length > 1) {
+        if (this._ingObjJustHaveOneValue(ingObj)) {
+          this._cleanRawBaseIngTreeRecur(ingObj[0])
+        } else {
+          this._workingBaseIngTree.push(ingObj)
+        }
       } else {
-        for (let ingArrayVal of ingArray) {
+        for (let ingArrayVal of ingObj) {
           this._cleanRawBaseIngTreeRecur(ingArrayVal)
         }
       }
     }
+  },
+  _ingObjIsStrArray (ingObj) {
+    for (let ingObjChild of ingObj) {
+      if (!StrHelper.isStrRobust(ingObjChild)) {
+        return false
+      }
+    }
+    return true
+  },
+  _ingObjJustHaveOneValue(ingObj) {
+    let compare = null
+    for (let ingObjChild of ingObj) {
+      if (compare) {
+        if (compare !== ingObjChild) {
+          return false
+        }
+      } else {
+        compare = ingObjChild
+      }
+    }
+    return true
   },
   _workingBaseIngTree: [],
   _workingVisitedIds: []
